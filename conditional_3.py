e0cf749492305bf6e2db4f18406955da22648017
arifasultana76 <arifa.rimi76@gmail.com>
2024-12-02 09:44:37 +0600

    single if or ternary operator

--- conditional_3.py
+++ conditional_3.py
@@ -1,13 +1,17 @@
 #A = 5 & G = M
 #A = 2 & G = F
 
-A = int (input("A : "))
+A = int(input("A : "))
 G = input("M/F : ") 
-if ((A == 1 or A == 2 ) and G == "M" ):
+
+if((A == 1 or A == 2 ) and G == "M"):
     print("fee is 100")
-elif (( A == 3 or A == 4) or G =="F" ):
+
+elif(( A == 3 or A == 4) or G == "F"):
     print("fee is 200")
-elif (A == 5 and G == "M"):
+
+elif(A == 5 and G == "M"):
     print("fee is 300")
+
 else:
     print("no fee")
---
+++ conditional_4.py
@@ -0,0 +1,8 @@
+# food = input(" food: ")
+# eat = "Yes" if food == "cake"  else "no" 
+# print(eat)
+
+food = input("food: ")
+print("sweet") if food == "cake" or food == "pudding" else print("not sweet")
+
+# this 2 program are single if /ternary operator statement 
