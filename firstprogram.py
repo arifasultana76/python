834758b343eca9c0aab4efa538caa4631c972b33
arifasultana76 <arifa.rimi76@gmail.com>
2024-12-03 19:39:30 +0600

    first some small program

---
+++ firstprogram.py
@@ -0,0 +1,29 @@
+## WAP to input side of a square & print its area:
+
+# side = int(input("enter side square:"))
+# print("area = ", side*4)
+
+# side = float(input("enter side square:"))
+# print("area = ", side*4)
+
+# side = int(input("enter side square:"))
+# print("area = ", side**2)
+
+# side = str(input("enter side name:"))
+# print("area name= ", side*4)
+
+## WAP to input 2 floating point numbers & print their average:
+
+# a = float(input("enter first:"))
+# b = float(input("enter second:"))
+# print("avg =",(a+b)/2)
+
+## WAP to input 2 int numbers, a and b. print True if a is greater than or equal to b.If not print False.
+
+a = int(input("enter first: "))
+b = int(input("enter second: "))
+print(a>=b)
+
+
+
+
