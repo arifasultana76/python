47a1b6a22dc54de044f31db3b7e1bef71e53fb23
arifasultana76 <arifa.rimi76@gmail.com>
2024-12-05 18:47:50 +0600

    Some type conversion file

--- type_conversion.py
+++ type_conversion.py
@@ -1,26 +1,26 @@
-# a = 5
-# b= 5.50
-# print(a+b)
+a = 5
+b= 5.50
+print(a+b)
 
-# a = " Rimi"
-# b = 50
-# c = (str(a))
+a = " Rimi"
+b = 50
+c = (str(a))
 
-# print(b*c)
-# a = float("5")
-# b = 6.62
-# print(a+b)
+print(b*c)
+a = float("5")
+b = 6.62
+print(a+b)
 
-# name = input("enter your name: ")
-# print("You're Strong", name)
+name = input("enter your name: ")
+print("You're Strong", name)
 
-# name= input("enter your name:")
-# age= input ("enter your age:")
-# marks= input("enter your marks:")
-# print("Hello!", name)
-# print("age = ",age)
-# print("marks = ",marks)
+name= input("enter your name:")
+age= input ("enter your age:")
+marks= input("enter your marks:")
+print("Hello!", name)
+print("age = ",age)
+print("marks = ",marks)
 
 first = int (input("enter first:"))
 second = int(input("enter second:"))
-print("sum :", first+second)
+print("sum :",first+second)
