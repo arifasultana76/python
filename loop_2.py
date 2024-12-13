3f6021efb741cde3ce6d645c40e3686853e2af42
arifasultana76 <arifa.rimi76@gmail.com>
2024-12-13 13:11:25 +0600

    For loop

--- loop_2.py
+++ loop_2.py
@@ -13,51 +13,51 @@
 
 ##code:
 
-# nums =[1,2,3,4,5]      ##list a for loop er use
+nums =[1,2,3,4,5]      ##list a for loop er use
 
-# for val in nums:
-#     print(val)
+for val in nums:
+    print(val)
 
 #Code:
 
-# vegetables = ["potato","cucumber","carrot"]               ##String a for loop er use
+vegetables = ["potato","cucumber","carrot"]               ##String a for loop er use
  
-# for val in vegetables:
-#     print(val)
+for val in vegetables:
+    print(val)
 
 
-# tup =(1,2,3,4,2,8,9)   ##tuple a loop er use
+tup =(1,2,3,4,2,8,9)   ##tuple a loop er use
 
-# for num in tup:
-#     print(num)
+for num in tup:
+    print(num)
 
 #Code:
 
-# str = "Arifa"
+str = "Arifa"
 
-# for char in str:
-#     print(char)
-
-#code:
-
-# str = "Arifa"
-
-# for char in str:            
-#     print(char)
-# print("End")
-
+for char in str:
+    print(char)
 
 #code:
 
-# str = "ArifaSultana"              ## i can't understand
+str = "Arifa"
 
-# for char in str:
-#     if(char == 'a'):
-#         print("a found")
+for char in str:            
+    print(char)
+print("End")
+
+
+#code:
+
+str = "ArifaSultana"           ## i can't understand
+
+for char in str:
+    if(char == 'a'):
+        print("a found")
        
-#     print(char)
-# else:
-#     print("end")
+    print(char)
+else:
+    print("end")
 
 #if i remove letter a then :
 
@@ -77,10 +77,10 @@ print(new_str)
 
 #Ans:
 
-# nums = [1,4,9,16,25,36,49,64,81,100]
+nums = [1,4,9,16,25,36,49,64,81,100]
 
-# for el in nums:
-#     print(el)
+for el in nums:
+    print(el)
 
 
 
@@ -88,24 +88,24 @@ print(new_str)
 
 #Ans:
 
-# nums = [1,4,9,16,25,36,49,64,81,100,49]
-# x = 49
-# idx = 0
-# for el in nums:
-#     if(el == x):
-#        print("number found of idx", idx)
-#     idx += 1
+nums = [1,4,9,16,25,36,49,64,81,100,49]
+x = 49
+idx = 0
+for el in nums:
+    if(el == x):
+       print("number found of idx", idx)
+    idx += 1
 
 #code:
 
-# nums = [1,4,9,16,25,36,49,64,81,100,49]
-# x = 49
-# idx = 0
-# for el in nums:
-#     if(el == x):
-#        print("number found of idx", idx)   #break use korle tokhn ekbar e idx ashbe
-#        break
-#     idx += 1
+nums = [1,4,9,16,25,36,49,64,81,100,49]
+x = 49
+idx = 0
+for el in nums:
+    if(el == x):
+       print("number found of idx", idx)   #break use korle tokhn ekbar e idx ashbe
+       break
+    idx += 1
 
 
 
