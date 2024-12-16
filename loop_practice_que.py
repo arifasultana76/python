44f9c6661c9d482ef86de803d74fa3f74e3455f6
arifasultana76 <arifa.rimi76@gmail.com>
2024-12-16 14:00:51 +0600

    Loop practice

--- loop_practice_que.py
+++ loop_practice_que.py
@@ -1,48 +1,49 @@
-#WAP to find the sum of first n numbers.(using while)
+# WAP to find the sum of first n numbers.(using while)
 
-# n = 5
+n = 5
 
-# for i in range(1, n+1):
-#     print(i)
-
-
-# n = 5
-
-# sum = 0
-# for i in range(1, n+1):
-#     sum += i
-
-# print("total sum =", sum)
-
-# n = 5
-
-# sum = 0                        ##While loop diye korte bolche
-# for i in range(1, n+1):     ## i can't understand
-#     sum += i
-
-# print("total sum =", sum)
+for i in range(1, n+1):
+    print(i)
 
 
-# n = 5
-# sum = 0
-# i = 1                       ## i didn't understand this code. 15 kivabe holo?
-# while i <= n:
-#     sum += i 
-#     i += 1   
+n = 5
 
-# print("total sum =", sum)
+sum = 0
+for i in range(1, n+1):
+    sum += i
+
+print("total sum =", sum)
+
+# 1+2+3+4+5
+n = 5
+
+sum = 0                         ##While loop diye korte bolche
+for i in range(1, n+1,1):        
+    sum += i
+print("total sum =", sum)
+
+#code:
+
+n = 5
+sum = 0
+i = 1                       
+while i <= n:
+    sum += i 
+    i += 1   
+
+print("total sum =", sum)
 
 #WAP to find the factorial of first n numbers.(using for)
 #Ans:
 
-# n = 5
-# fact = 1
-# i = 1                       
-# while i <= n:             #3While loop diye code
-#     fact *= i 
-#     i += 1   
+n = 5
+fact = 1
+i = 1                       
+while i <= n:             #While loop diye code
+    fact *= i 
+    i += 1   
 
-# print("factorial =", fact)
+print("factorial =", fact)
 
 #or
 
