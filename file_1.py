d509fc4bf75f1b0fb774e25d2a7706d1b21cc287
arifasultana76 <arifa.rimi76@gmail.com>
2024-12-17 15:32:50 +0600

    File remove method

---
+++ file_1.py
@@ -0,0 +1,9 @@
+#  Deleting a file:
+# using the os module
+# Module (like a code library) is a file written by another
+# programmer that generally has a functions we can use.
+# import os
+
+import os
+
+os.remove("sample.txt")
